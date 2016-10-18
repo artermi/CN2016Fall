09e69d716ede77f5ddc282a55bd664932dda8621
Artermi <ericcheng230@gmail.com>
2016-10-19 02:05:00 +0800

    initial

---
+++ project1/client.c
@@ -0,0 +1,39 @@
+#include "common.h"
+
+int main(int argc, char* argv[]){
+//./client [-n number] [-t time] host_1:IP host_2:IP 
+	int num = 0,tout = 1000;//int number and timeout
+	int host_num = 0;
+	char* host[argc];
+	for(int i = 0; i < argc; i++){
+		if(strcmp(argv[i],"-n") == 0)
+			num = atoi(argv[i+1]);
+
+		else if(strcmp(argv[i],"-t") == 0)
+			tout = atoi(argv[i+1]);
+
+		else if(strncmp(argv[i],"host",4) == 0){
+			int stlength = strlen(argv[i]);
+			for(int k = 1; k < stlength; k++){
+				if(*argv[i] != ':'){
+					printf("%d %d %c\n",argv[i],&(argv[i][0]),argv[i][0]);
+					argv[i] ++;
+				}
+				else{
+					argv[i] ++;
+					break;
+				}
+			}
+
+			host[host_num] = argv[i];
+			host_num ++;
+		}
+	}
+	while(1){
+		int j;
+		scanf("%d",&j);
+		printf("%s\n",host[j]);
+	}
+	
+	return 0;
+}
---
+++ project1/common.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <utime.h>
+
+
