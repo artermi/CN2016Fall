8006f56d721ed89acae8f73064b5ecfdbdf694c4
artermi <ericcheng230@gmail.com>
2016-10-21 14:20:44 +0800

    tmp

--- project1/client.c
+++ project1/client.c
@@ -7,27 +7,17 @@ int main(int argc, char* argv[]){
 	char* host[argc];
 	for(int i = 0; i < argc; i++){
 		if(strcmp(argv[i],"-n") == 0)
-			num = atoi(argv[i+1]);
+			num = atoi(argv[++i]);
 
 		else if(strcmp(argv[i],"-t") == 0)
-			tout = atoi(argv[i+1]);
+			tout = atoi(argv[++i]);
 
 		else if(strncmp(argv[i],"host",4) == 0){
-			int stlength = strlen(argv[i]);
-			for(int k = 1; k < stlength; k++){
-				if(*argv[i] != ':'){
-//					printf("%d %d %c\n",argv[i],&(argv[i][0]),argv[i][0]);
-					argv[i] ++;
-				}
-				else{
-					argv[i] ++;
-					break;
-				}
-			}
 			host[host_num] = argv[i];
 			host_num ++;
 		}
 	}
+	printf("%d %d\n",num,tout);
 
 
 	
