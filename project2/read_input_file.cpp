022e59a6804d19ac6048cca009ef4c44d2fafe09
artermi <ericcheng230@gmail.com>
2016-12-08 15:06:24 +0800

    read_input_file has done

--- project2/read_input_file.cpp
+++ project2/read_input_file.cpp
@@ -17,7 +17,63 @@ void read_input_file(string file_name,string &id,string &pass, vector<class arti
 
 	cout << input << endl;
 //	wstring winput(input.begin(),input.end());
+	for(int i = 0; i < input.size(); i ++){
+		if(input.compare(i,4,"<ID>") == 0){
+			i += 4;
+			while(input.compare(i,5,"</ID>")){
+				id += input[i];
+				i++;
+			}
+		}
+		if(input.compare(i,6,"<PASS>") == 0){
+			i += 6;
+			while(input.compare(i,7,"</PASS>")){
+				pass += input[i];
+				i++;
+			}
+		}
+		if(input.compare(i,7,"<BOARD>") == 0){
+			i += 7;
+			class  article new_article;
+			while(input.compare(i,8,"</BOARD>")){
+				new_article.board += input[i];
+				i++;
+			}
+			while(input.compare(i,3,"<P>"))
+				i++;
+			i+=3;
+			while(input.compare(i,4,"</P>")){
+				new_article.title += input[i];
+				i++;
+			}
+			while(input.compare(i,9,"<CONTENT>"))
+				i++;
+			i+=9;
+			while(input.compare(i,10,"</CONTENT>")){
+				new_article.content += input[i];
+				i++;
+			}
+			post.push_back(new_article);
 
+		}
+		if(input.compare(i,6,"<EXIT>") == 0){
+			i += 6;
+			exit = true;
+			break;
+		}
+	}
+	cout << id <<endl;
+	cout << pass <<endl;
+	for(int i = 0; i < post.size(); i++){
+		cout << "*******" << endl;
+		cout << "board: " << post[i].board << endl;
+		cout << "title: " << post[i].title << endl;
+		cout << "content:" <<endl;
+		cout << post[i].content << endl;
+	}
+	cout << exit <<endl;
+
+/*
 	for(int i=0; i < input.size();i++){
 		if(input[i] < 0){
 			cout <<input[i] << input[i+1] << input[i+2] <<endl;
@@ -30,4 +86,5 @@ void read_input_file(string file_name,string &id,string &pass, vector<class arti
 		else
 			cout << input[i] << endl;
 	}
+	*/
 }
