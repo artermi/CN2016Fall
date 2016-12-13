4e42932c44b5878b4c1a5ce4761b28e74794e0e9
artermi <ericcheng230@gmail.com>
2016-12-13 13:43:05 +0800

    verify to multiple user mode

--- project2/test_read_input_file.cpp
+++ project2/test_read_input_file.cpp
@@ -1,13 +1,9 @@
 #include "read_input_file.h"
 
 int main(int argc,char** argv){
-	string ID,PASS;
-	vector<article> POST;
-	bool exit;
+	vector<class user> users;
 
 	string file_name = "P_input.txt";
-	if(argc > 1)
-		file_name = string(argv[1]);
-	read_input_file(file_name,ID,PASS,POST,exit);
+	read_input_file(users,file_name);
 	return 0;
 }
