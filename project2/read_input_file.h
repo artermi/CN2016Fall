5804210eb6cce3ef209840fe87b3b72fa35aa41a
artermi <ericcheng230@gmail.com>
2017-01-16 09:45:40 +0800

    whf??

--- project2/read_input_file.h
+++ project2/read_input_file.h
@@ -1,7 +1,6 @@
 #include "classes.h"
 #include <iostream>
 #include <fstream>
-#include <vector>
 using namespace std;
 
-void read_input_file(string file_name,string &id,string &pass, vector<class article> &post, bool &exit);
+void read_input_file(vector<user> &user_list,string file_name);
